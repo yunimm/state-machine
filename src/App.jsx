c03e9feed16aef82fcbee736d8611d680dac05e2
yunimm <yuu9916@gmail.com>
2024-07-17 02:44:49 +0800

    wip: add select current machine ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
+import React, { useState } from 'react';
 import Fetch from './Fetch';
 import Toggler from './Toggler';
 import FetchDog from './FetchDog';
 import RetryPromise from './RetryPromise';
+import { Select, SelectItem } from "@nextui-org/react";
+
+const machines = [
+  { key: "Toggler", label: "Toggler" },
+  { key: "fetch", label: "fetch" },
+  { key: "FetchDog", label: "FetchDog" },
+  { key: "RetryPromise", label: "RetryPromise" },
+];
 
 function App() {
-    return (
-        <>
-          {/* <Fetch /> */}
-          {/* <FetchDog /> */}
-          {/* <Toggler /> */}
-          <RetryPromise />
-        </>
-    );
+  const [selectedMachine, setSelectedMachine] = useState(machines[0].key);
+
+  const renderComponent = () => {
+    switch (selectedMachine) {
+      case 'Toggler':
+        return <Toggler />;
+      case 'fetch':
+        return <Fetch />;
+      case 'FetchDog':
+        return <FetchDog />;
+      case 'RetryPromise':
+        return <RetryPromise />;
+      default:
+        return null;
+    }
+  };
+  return (
+    // FIXME: 預設 label 顯示 toggler
+    <>
+      <Select
+        label="Select an machine"
+        className="max-w-xs"
+        onChange={(e) => setSelectedMachine(e.target.value)}
+      >
+        {machines.map((machine) => (
+          <SelectItem key={machine.key}>
+            {machine.label}
+          </SelectItem>
+        ))}
+      </Select>
+      <br />
+      {renderComponent()}
+    </>
+  );
 }
 
 export default App;
