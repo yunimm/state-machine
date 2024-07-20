1292ed48cf908fbd0a197c160785202ff3c7bad8
yunimm <yuu9916@gmail.com>
2024-07-21 04:09:17 +0800

    feat: update sidebarDate and modify style

--- src/components/NavigationColumn.jsx
+++ src/components/NavigationColumn.jsx
@@ -29,8 +29,8 @@ const Section = ({ title, items, activeItem, onItemClick }) => (
 
 // SidebarList 組件
 const SidebarList = ({ sections, activeItem, onItemClick }) => (
-  <div className="bg-gray-900 text-gray-400 p-4 w-64">
-    <h2 className="text-white text-xl font-bold mb-4">Docs02</h2>
+  <div className="bg-gray-800 text-gray-400 p-4 w-64">
+    <h2 className="py-1 px-2 text-gray-400 mb-4">StateMachine</h2>
     <ul className="space-y-6">
       {sections.map((section, index) => (
         <Section
@@ -45,8 +45,8 @@ const SidebarList = ({ sections, activeItem, onItemClick }) => (
   </div>
 );
 
-export default function NavigationColumn({ target }) {
-  const [activeItem, setActiveItem] = useState(target);
+export default function NavigationColumn() {
+  const [activeItem, setActiveItem] = useState("Basics-toggle");
 
   const handleItemClick = (item) => {
     setActiveItem(item);
@@ -55,31 +55,21 @@ export default function NavigationColumn({ target }) {
   const sidebarData = [
     {
       title: "Basics",
-      items: ["Lorem Ipsum", "Lorem Ipsum 1", "Lorem Ipsum 2", "Lorem Ipsum 3", "Lorem Ipsum 4", "Lorem Ipsum 5"]
+      items: ["toggle"]
     },
     {
-      title: "Header 1",
-      items: ["Lorem Ipsum", "Lorem Ipsum 1"]
+      title: "Fetch",
+      items: ["RetryPromise"]
     },
-    {
-      title: "Header 2",
-      items: ["Lorem ipsum", "Lorem Ipsum 1"]
-    },
-    {
-      title: "Header 3",
-      items: ["Lorem ipsum", "Lorem Ipsum 1"]
-    },
-    {
-      title: "Header 4",
-      items: ["Lorem Ipsum", "Lorem Ipsum 1"]
-    }
   ];
 
   return (
-    <SidebarList
-      sections={sidebarData}
-      activeItem={activeItem}
-      onItemClick={handleItemClick}
-    />
+    <>
+      <SidebarList
+        sections={sidebarData}
+        activeItem={activeItem}
+        onItemClick={handleItemClick}
+      />
+    </>
   );
 }
